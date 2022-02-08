6a23a9a93e65828759ddb416faa1261862343675
hartaithan <hartaithan@gmail.com>
2022-02-08 10:22:04 +0900

    refactor index.js

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,53 @@
-import React, useState, useEffect from "react";
-import Grid from "@material-ui/core";
+import React, { useState, useEffect } from "react";
+import Grid from "@mui/material/Grid";
 
-export default function Page(id, options, count, color, data) {
-  return <MyWonderfulComponent id="id" options="options" count="count" color="color" data="data">I'm text from a component</MyWonderfulComponent>
+export default function Page() {
+  const id = 0;
+  const options = {
+    params: {
+      fields: {
+        isDynamic: true,
+      },
+    },
+  };
+  const count = 0;
+  const color = "red";
+  const data = {};
+  return (
+    <MyWonderfulComponent
+      id={id}
+      options={options}
+      count={count}
+      color={color}
+      data={data}
+    >
+      I&apos;m text from a component
+    </MyWonderfulComponent>
+  );
 }
 
-function MyWonderfulComponent(id, options, children, other) {
-  const { count } = other;
-  const { summ, setSumm } = useState(count);
+function MyWonderfulComponent({ id, options, count, color, data, children }) {
+  const [summ, setSumm] = useState(count);
 
   useEffect(() => {
-    if (id && options && options.params && options.params.fields && options.params.fields.isDynamic) {
+    if (
+      id &&
+      options &&
+      options.params &&
+      options.params.fields &&
+      options.params.fields.isDynamic
+    ) {
       setSumm(summ + 1);
     }
-  });
+  }, []); // eslint-disable-line
 
   return (
-    <h1>Hello World!</h1>
-    <Grid>
-      <Grid xs={12}>{children}</Grid>
-      <Grid>{ summ }</Grid>
-    </Grid>
+    <>
+      <h1>Hello World!</h1>
+      <Grid>
+        <Grid xs={12}>{children}</Grid>
+        <Grid>{summ}</Grid>
+      </Grid>
+    </>
   );
 }
